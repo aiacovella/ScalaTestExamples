c14e1d06c23ccabd7d53218cf5ea3112597c4e50
aiacovella <aiacovella@chariotsolutions.com>
2010-07-26 20:03:33 -0400

    Added test scope

--- src/test/scala/com/foo/FooTest.scala
+++ src/test/scala/com/foo/FooTest.scala
@@ -4,12 +4,7 @@ import org.junit.{Assert, Test}
 
 class FooTest{
 
-  @Test { val expected = classOf[ IllegalArgumentException] }
-  def shouldThrowException {
-      Foo.methodThatThrowsException
-  }
-
-  object Foo{
-    def methodThatThrowsException() = throw new NullPointerException
+  @Test
+  def something {
   }
 }
